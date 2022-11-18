2acd822e971ba650eb3a16f1cb7aecb0acdc9570
sshiktarev <sshiktarev@web3tech.ru>
2022-11-18 15:48:50 +0000

    change compiler

--- build.sh
+++ build.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
-sourcefile=python-helm-vault.py
-binfile=python-helm-vault
+python3 -m pip install pyinstaller
 
-python3 -m pip install cython
+pyinstaller python-helm-vault.py --onefile
 
-cython $sourcefile --embed
-
-PYTHONLIBVER=python$(python3 -c 'import sys; print(".".join(map(str, sys.version_info[:2])))')$(python3-config --abiflags)
-
-gcc -Os $(python3-config --includes) ${binfile}.c -o $binfile $(python3-config --ldflags) -l$PYTHONLIBVER
+cp ./dist/* ./
